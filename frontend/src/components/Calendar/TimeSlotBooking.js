46c17c77a8a4a3b88752847dc5ca4bdd0ff1b5f8
mpvnlv <v.kruk@innopolis.university>
2023-06-20 10:33:23 +0300

    -

--- frontend/src/components/Calendar/TimeSlotBooking.js
+++ frontend/src/components/Calendar/TimeSlotBooking.js
@@ -2,35 +2,36 @@ import { useState } from "react";
 export const TimeSlotBooking = () => {
   const slots = ["NO SLOT", "FREE", "BOOKED"];
   const colors = ["bg-slot_no", "bg-slot_free", "bg-booked_normal"];
-  
-  var [count, setCount ] = useState(0)
+
+  var [count, setCount] = useState(0)
   // const [users, setUsers] = useState([])
-  if (count > 1){
+  if (count > 1) {
     count = 0;
   }
   const postData = async (url = '', data = {}) => {
-     
+
     // Формируем запрос
     const response = await fetch(url, {
       // Метод, если не указывать, будет использоваться GET
       method: 'POST',
-      headers: {"Content-type":"application/json",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Headers": "content-type",
-     },
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "content-type",
+      },
       // Данные
       body: JSON.stringify(data)
     });
-    return response; 
+    return response;
   }
   var fullname = "";
-  
-  const get_info = () =>{
+
+  const get_info = () => {
     // setCount(count + 1);
     const data_student = {
-      "type":"log",
-      "mail":"ekjfnvjer",
-      "password":"123456"
+      "type": "log",
+      "mail": "ekjfnvjer",
+      "password": "123456"
     }
     postData('https://testing.egorleb.repl.co', data_student).then(async (data_) => {
       const response = await data_.json();
@@ -39,32 +40,32 @@ export const TimeSlotBooking = () => {
       return response;
     });
   }
-  const sendFreeTime = () =>{
+  const sendFreeTime = () => {
     // setCount(count+1)
     const data = {
-      "type":"setTime",
-      "mail":"ekjfnvjer",//видимо будем хранить в локалке данные 
-      "password":"123456",
-      "freeSlots":"[[datetime, datetime], [datetime, datetime], ..., [datetime, datetime]]",
-      "busySlots":"[[datetime, datetime], [datetime, datetime], ..., [datetime, datetime]]"
+      "type": "setTime",
+      "mail": "ekjfnvjer",//видимо будем хранить в локалке данные 
+      "password": "123456",
+      "freeSlots": "[[datetime, datetime], [datetime, datetime], ..., [datetime, datetime]]",
+      "busySlots": "[[datetime, datetime], [datetime, datetime], ..., [datetime, datetime]]"
     }
-      postData('https://testing.egorleb.repl.co', data).then(async ( data_) => {
-        const res = await data_.json();
-        console.log("1")
-      });
-     
-}
-  if (count == 1 || count == 0){
-  return (
-       <div className="group p-1 border-header_border border-2 relative">
-        <div
-          onClick={count == 0 ? sendFreeTime() : setCount(count+1)}
-          className={`pb-20 hover:bg-no_slot_hover hover:cursor-pointer rounded-md  ${colors[count]}`}>
-            <p></p>
-        </div>
-      </div>
-    );
-   }
+    postData('https://testing.egorleb.repl.co', data).then(async (data_) => {
+      const res = await data_.json();
+      console.log("1")
+    });
+
+  }
+  // if (count == 1 || count == 0){
+  // return (
+  //      <div className="group p-1 border-header_border border-2 relative">
+  //       <div
+  //         onClick={count == 0 ? sendFreeTime() : setCount(count+1)}
+  //         className={`pb-20 hover:bg-no_slot_hover hover:cursor-pointer rounded-md  ${colors[count]}`}>
+  //           <p></p>
+  //       </div>
+  //     </div>
+  //   );
+  //  }
   // else if (count == 0){
   //   return (
   //     <div className="group p-1 border-header_border border-2 relative">
@@ -77,21 +78,22 @@ export const TimeSlotBooking = () => {
   //     </div>
   //   );
   // }
-  else{
-    return (
-      <div className="group p-1 border-header_border border-2 relative">
-        <div
-          onClick={get_info}
-          className={`pb-20 hover:bg-no_slot_hover hover:cursor-pointer rounded-md  ${colors[count]}`}
-        ></div>
-        <div
-          className="flex flex-col justify-center items-center  absolute -top-14 -right-28 group-hover:visible invisible  bg-white border-booked_clicked border-2 py-4 px-10 z-20
+
+  return (
+    <div className="group p-1 border-header_border border-2 relative">
+      <div
+        onClick={count == 0 ? sendFreeTime() : (count == 1 ? setCount(count + 1) : get_info())}
+        className={`pb-20 hover:bg-no_slot_hover hover:cursor-pointer rounded-md  ${colors[count]}`}
+      ></div>
+      {count == 2 ? <div
+        className="flex flex-col justify-center items-center  absolute -top-14 -right-28 group-hover:visible invisible  bg-white border-booked_clicked border-2 py-4 px-10 z-20
       rounded-tl-lg rounded-tr-lg rounded-br-lg text-booked_clicked"
-        >
-          <p className="z-30" >{fullname}</p>
-          <p className="z-30">тут мне с сервера пришлют номер телефона</p>
-        </div>
-      </div>
-    );
-  }
-};
+      >
+        <p className="z-30" >{fullname}</p>
+        <p className="z-30">тут мне с сервера пришлют номер телефона</p>
+      </div> : <></>}
+
+    </div>
+  );
+}
+
