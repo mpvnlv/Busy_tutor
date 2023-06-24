80929cc9daeb32297e32f142c6a6b06beb69fb5b
mpvnlv <88908152+mpvnlv@users.noreply.github.com>
2023-06-24 16:36:51 +0300

    Update main.py
    add a functions to work with database

--- main.py
+++ main.py
@@ -4,44 +4,133 @@ import json
 
 
 def log(mail, password):
-    pass
+    owner = checkInOwners(mail, password)
+    if len(owner) != 0:
+        return {
+            "role": "owner",
+            "mail": owner[0][0],
+            "fullname": owner[0][2],
+            "freeSlots": owner[0][3],
+            "busySlots": owner[0][4],
+            "phone": owner[0][5]
+        }
+    visitor = checkInVisitors(mail, password)
+    if len(visitor) != 0:
+        return {
+            "role": "visitor",
+            "mail": visitor[0][0],
+            "fullname": visitor[0][2],
+            "ownerMail": visitor[0][3],
+            "phone": visitor[0][4]
+        }
+    return {}
 
 
 def reg(mail, password, role, fullname, phone, ownerMail=''):
-    pass
+    if role == 'owner' and len(checkInOwnersOnlyMail(mail)) == 0:
+        cursor.execute(f"""
+                        INSERT INTO owners VALUES ('{mail}', '{password}', '{fullname}', '[]', '[]', '{phone}')
+                        """)
+        connection.commit()
+    elif role == "visitor" and len(checkInVisitorsOnlyMail(mail)) == 0:
+        cursor.execute(f"""
+                        INSERT INTO visitors VALUES ('{mail}', '{password}', '{fullname}', '{ownerMail}', '{phone}')
+                        """)
+        connection.commit()
+    else:
+        return 405
+    return 200
 
 
 def checkInOwners(mail, password):
-    pass
+    cursor.execute(f"""
+            SELECT * FROM owners
+            WHERE mail = '{mail}' and password = '{password}'
+            """)
+    rows = cursor.fetchall()
+    return rows
 
 
 def checkInOwnersOnlyMail(mail):
-    pass
+    cursor.execute(f"""
+            SELECT * FROM owners
+            WHERE mail = '{mail}'
+            """)
+    rows = cursor.fetchall()
+    return rows
 
 
 def checkInVisitors(mail, password):
-    pass
+    cursor.execute(f"""
+            SELECT * FROM visitors
+            WHERE mail = '{mail}' and password = '{password}'
+            """)
+    rows = cursor.fetchall()
+    return rows
 
 
 def checkInVisitorsOnlyMail(mail):
-    pass
+    cursor.execute(f"""
+            SELECT * FROM visitors
+            WHERE mail = '{mail}'
+            """)
+    rows = cursor.fetchall()
+    return rows
 
 
 def getTime(mail):
-    pass
+    cursor.execute(f"""
+                SELECT * FROM owners
+                WHERE mail = '{mail}'
+                """)
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        return None
+    return {
+        "freeSlots": rows[0][3],
+        "busySlots": rows[0][4]
+    }
 
 
 def setFreeSlots(mail, password, data):
-    pass
+    cursor.execute(f"""UPDATE owners SET freeSlots = '{data}' where mail = '{mail}' and password = '{password}'""")
+    connection.commit()
 
 
 def setBusySlots(mail, password, data):
-    pass
+    cursor.execute(f"""UPDATE owners SET busySlots = '{data}' where mail = '{mail}' and password = '{password}'""")
+    connection.commit()
+
+
+def cleaner():
+    cursor.execute("""
+                DROP TABLE owners;
+                """)
+    cursor.execute("""
+                DROP TABLE visitors;
+                """)
+    connection.commit()
 
 
 def getInfo(mail):
-    pass
-
+    owner = checkInOwnersOnlyMail(mail)
+    if len(owner) != 0:
+        return {
+            "role": "owner",
+            "mail": owner[0][0],
+            "fullname": owner[0][2],
+            "freeSlots": owner[0][3],
+            "busySlots": owner[0][4]
+        }
+    visitor = checkInVisitorsOnlyMail(mail)
+    if len(visitor) != 0:
+        return {
+            "role": "visitor",
+            "mail": visitor[0][0],
+            "fullname": visitor[0][2],
+            "ownerMail": visitor[0][3]
+        }
+    return None
 
 connection = sqlite3.connect('mydatabase.db')
 cursor = connection.cursor()
@@ -70,28 +159,4 @@ CREATE TABLE IF NOT EXISTS visitors (
 """)
 connection.commit()
 
-server = socket.create_server(("", 2000))
-server.listen(100)
-cn = 0
-while True:
-    cn += 1
-    content, address = server.accept()
-
-    request = content.recv(102400).decode().split('\n')
-    print(f"--------------------{cn}--------------------")
-    print(request[0])
-
-    method, url, prot = request[0].split()
-
-    code_response = 200
-    res = {}
-
-
-    connection.commit()
-    print(f"--------------------{cn}--------------------")
-    print()
-    head = f'HTTP/1.1 {code_response} {"OK" if code_response == 200 else "FAIL"}\r\n' \
-           f'Content-Type: application/json; charset=utf-8\r\nAccess-Control-Allow-Origin: *\r\n' \
-           f'Access-Control-Allow-Headers: *\r\n\r\n'
-    content.send(head.encode() + json.dumps(res).encode())
-    content.close()
+print(getInfo("vikalol2003"))
